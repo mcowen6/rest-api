9d748595ac201bb40b43ac06d315418c45e2b27d
Max Cowen-Carr <maxacowencarr@icloud.com>
2022-12-21 10:58:51 +0000

    added schema

--- src/users/userModel.js
+++ src/users/userModel.js
@@ -1,3 +1,21 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const Users2 = mongoose.model("Users2", userSchema);
+module.exports = Users2;
